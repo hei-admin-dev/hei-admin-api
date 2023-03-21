6c7eb0af1cc9612aedad186f23082fcc112371a1
zoherifitia <hei.nitsiaro@gmail.com>
2023-03-21 10:40:16 +0300

    fix: find all courses

--- src/main/java/school/hei/haapi/service/CourseService.java
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -1,8 +1,12 @@
 package school.hei.haapi.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import school.hei.haapi.model.BoundedPageSize;
 import school.hei.haapi.model.Course;
+import school.hei.haapi.model.PageFromOne;
 import school.hei.haapi.model.validator.CourseValidator;
 import school.hei.haapi.repository.CourseRepository;
 
@@ -14,6 +18,14 @@ import java.util.List;
 public class CourseService {
     private CourseRepository courseRepository;
     private CourseValidator courseValidator;
+
+    public List<Course> getAll(PageFromOne page, BoundedPageSize pageSize){
+        Pageable pageable = PageRequest.of(
+                page.getValue() - 1,
+                pageSize.getValue()
+        );
+        return courseRepository.findAll(pageable).getContent();
+    }
 
     @Transactional
     public List<Course> saveAll(List<Course> courses) {
