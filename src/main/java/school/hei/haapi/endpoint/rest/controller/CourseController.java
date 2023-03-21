e617d47f6775dc6ba5422458df9cbc387e5fa691
zoherifitia <hei.nitsiaro@gmail.com>
2023-03-21 10:53:45 +0300

    chore: GET /courses controller

--- src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
@@ -1,12 +1,12 @@
 package school.hei.haapi.endpoint.rest.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.hei.haapi.endpoint.rest.mapper.CourseMapper;
 import school.hei.haapi.endpoint.rest.model.Course;
 import school.hei.haapi.endpoint.rest.model.CrupdateCourse;
+import school.hei.haapi.model.BoundedPageSize;
+import school.hei.haapi.model.PageFromOne;
 import school.hei.haapi.service.CourseService;
 
 import java.util.List;
@@ -17,6 +17,15 @@ import java.util.stream.Collectors;
 public class CourseController {
     private final CourseService courseService;
     private final CourseMapper courseMapper;
+
+    @GetMapping(value = "/courses")
+    public List<Course> getAllCourses(
+            @RequestParam("page") PageFromOne page,
+            @RequestParam("page_size") BoundedPageSize pageSize){
+        return courseService.getAll(page,pageSize).stream()
+                .map(courseMapper::toRestCourse)
+                .collect(Collectors.toUnmodifiableList());
+    }
 
     @PutMapping(value = "/courses")
     public List<Course> createOrUpdateCourses(@RequestBody List<CrupdateCourse> toWrite) {
